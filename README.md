c6f28c7606f234ff4f4bc5f11d8b35d900ab9fe7
medinae <akb.dubai@gmail.com>
2016-10-21 00:57:38 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Go playground
+=============
+
+Execute the command :
+
+    go run hello.go
+    
+[![Terminal](https://s15.postimg.org/64g77mj1n/terminal.jpg)](https://postimg.org/image/nuhvsnwmf/)
---
+++ hello.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+
+type shape interface {
+	draw()
+}
+
+type rect struct {
+	name string
+	height int
+}
+
+func (re rect) draw() {
+	var height = re.height
+
+	if height > 33 {
+		height = 33
+	}
+
+	drawRightAngledTriangle(height)
+}
+
+func drawRightAngledTriangle(max int) {
+	for i := 1; i <= max; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+
+		fmt.Println()
+	}
+}
+
+func main() {
+	fmt.Println("Please choose a height for your lovely triangle : ")
+
+	var inputSize int
+	fmt.Scanln(&inputSize)
+
+	var myShape = rect{"right-angled", inputSize}
+	myShape.draw()
+}
